e0e520d4787cca8bf752016acc9b47c38e18bbba
MatiasUC <mtoro13@hotmail.com>
2013-09-10 23:07:57 -0300

    Commit inicial

---
+++ SGHWeb/SGHWeb/Models/LoginModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SGHWeb.Models
+{
+    public class LoginSGHModel
+    {
+        [Required(ErrorMessage = "Debe ingresar el Usuario")]
+        [Display(Name="Usuario")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la Contraseña")]
+        [DataType(DataType.Password)]
+        [Display(Name="Contraseña")]
+        public string password { get; set; }
+    }
+}
